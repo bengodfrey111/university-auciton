cf61e6de95faaa24b6b3488fda5e855955543f9a
godfreyb2 <godfreyb2@coventry.ac.uk>
2021-11-09 19:21:28 +0000

    datetime is now stored in Items

--- storeItems.py
+++ storeItems.py
@@ -1,8 +1,9 @@
 import sqlite3
+import datetime
 
 def newItem(itemName, description, username, image): #this will just allow the user to add a new item
     connection = sqlite3.connect("AuctionDB")
-    cursor = connection.execute("INSERT INTO Items (name, description, username) VALUES (:newName, :newDescription, :newUsername)", {"newDescription": description, "newName": itemName, "newUsername": username})
+    cursor = connection.execute("INSERT INTO Items (name, description, username, datetime) VALUES (:newName, :newDescription, :newUsername, :cDateTime)", {"newDescription": description, "newName": itemName, "newUsername": username, "cDateTime": datetime.datetime.now()})
     SecondCursor = connection.execute("SELECT last_insert_rowid()") #learn sql command in https://alvinalexander.com/android/sqlite-autoincrement-insert-value-primary-key/
     primaryID = 0
     for row in SecondCursor:
@@ -10,3 +11,16 @@ def newItem(itemName, description, username, image): #this will just allow the u
     connection.commit()
     connection.close()
     image.save("static/itemImages/" + str(primaryID)) #image is going to be saved using the primary key
+
+def item(ID):
+    connection = sqlite3.connect("AuctionDB")
+    cursor = connection.execute("SELECT * FROM Items WHERE ID=:pID", {"pID": ID})
+    rawResult = []
+    for row in cursor:
+        rawResult = row
+    connection.close()
+    result = {"name": row[1], "description": row[2], "username": row[3], "datetime": datetime.datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f')}
+    return result
+
+if __name__ == "__main__":
+    print(item(1))
