1751af4ad043a73e45483a4a5ae0cd24ae14e95a
godfreyb2 <godfreyb2@coventry.ac.uk>
2021-11-11 18:08:11 +0000

    implemented sessions, may need to test it slightly just in case but will do that later

---
+++ sessions.py
@@ -0,0 +1,3 @@
+import sqlite3
+
+#autoincrement the session id
--- start.py
+++ start.py
@@ -4,12 +4,14 @@ from flask import request
 from flask import make_response
 from flask import render_template
 from flask import session
+
 import datetime
 
 import login #my login code
 import storeItems
 
 app = Flask(__name__)
+app.secret_key = "zOGSyUx5ctSTf2UXwxBr" #just some random string to make the session key unpredictable
 
 def stringDate(dateTime): #this simply makes the date time a string so that I can just stick it on a HTML page in a readable state
     return str(dateTime.day) + "/" + str(dateTime.month) + "/" + str(dateTime.year)
@@ -21,13 +23,16 @@ def htmlListItems(items): #this lists a bunch of items viewable in html
     return html
 
 
+@app.route("/logout")
+def logout():
+    session.pop("username", default=None)
+    return "<script>window.location.replace('/');</script>"
+
 @app.route("/")
 def index():
     html = "<!DOCTYPE html>"
-    username = request.cookies.get('username')
-    password = request.cookies.get('password')
-    if login.login(username, password):
-        html+= "<button onclick=logout()>Logout</button><br><br> <script>function logout(){document.cookie=''; window.location.replace('/');}</script>"
+    if session.get("username"):
+        html+= "<button onclick=logout()>Logout</button><br><br> <script>function logout(){document.cookie=''; window.location.replace('/logout');}</script>"
     else:
         html+= "<a href='/login'><small>login</small></a><br><br>"
 
@@ -42,7 +47,8 @@ def loginPage(validCredentials = ""):
         username = request.form["username"]
         password = request.form["password"]
         if login.login(username, password): #if login credentials are valid then goes to right page, else goes back to log in so that they can put in valid credentials
-            return "<script>window.location.replace('/home')</script>"
+            session["username"] = username
+            return "<script>window.location.replace('/')</script>"
         else:
             return render_template("login.html", validUser="!") #send login page up with error message if login is invalid
     return render_template("login.html", validUser=validCredentials) #just displays the login page
@@ -62,9 +68,7 @@ def newUser():
 
 @app.route("/home")
 def home():
-    username = request.cookies.get('username') #gets the user credentials so that it knows that the user is logged in
-    password = request.cookies.get('password')
-    if login.login(username, password): #if login credentials are valid then goes to right page, else goes back to log in so that they can put in valid credentials
+    if session.get("username"): #checks if user is logged in
         return render_template("home.html")
     else:
         return loginPage("!")
@@ -72,15 +76,13 @@ def home():
 
 @app.route("/newItem", methods=["GET", "POST"])
 def newItem():
-    username = request.cookies.get('username') #gets the user credentials so that it knows that the user is logged in
-    password = request.cookies.get('password')
-    if login.login(username, password):
+    if session.get("username"): #checks if user is logged in:
         if request.method == "POST":
             description = request.form["description"] #obtains the image, description and item name the user puts in
             itemName = request.form["itemName"]
             if request.files: #checks if user is trying to upload a file (if not it would just display the new item page without doing anything)
                 image = request.files["image"] #learnt how to handle images at https://pythonise.com/series/learning-flask/flask-uploading-files
-                storeItems.newItem(itemName, description, username, image) #filename is the primary key ID
+                storeItems.newItem(itemName, description, session["username"], image) #filename is the primary key ID
                 return home()
             
 
@@ -93,9 +95,7 @@ def newItem():
 def item(ID): #this simply displays the item using its unique ID
     itemAtr = storeItems.item(ID)
     if itemAtr != None: #checks if the item exists, if it exists then it will go to the normal page, if it doesn't then it will go to a page explaining that the item was not found
-        username = request.cookies.get('username')
-        password = request.cookies.get('password')
-        if login.login(username, password):
+        if session.get("username"): #checks if user is logged in
             return render_template("item.html", idImage=ID, name=itemAtr["name"], user=itemAtr["username"], description=itemAtr["description"], dateTime=stringDate(itemAtr["datetime"]), login="")
         else:
             return render_template("item.html", idImage=ID, name=itemAtr["name"], user=itemAtr["username"], description=itemAtr["description"], dateTime=stringDate(itemAtr["datetime"]), login="!")
@@ -105,10 +105,8 @@ def item(ID): #this simply displays the item using its unique ID
 
 @app.route("/myItems")
 def myItems():
-    username = request.cookies.get('username')
-    password = request.cookies.get('password')
-    if login.login(username, password): #checks if user is logged in
-        items = storeItems.myItems(username) #gets all the items a specific user is selling
+    if session.get("username"): #checks if user is logged in
+        items = storeItems.myItems(session["username"]) #gets all the items a specific user is selling
         html = "<!DOCTYPE html>"
         html+= htmlListItems(items)
         return html
